0b664776aa0a55865b3aa507946c0c4772e8cd00
gyojincompany <luckyred@gmail.com>
2022-10-12 12:58:37 +0900

    jsp java beans 연습

---
+++ src/main/java/com/gyojincompany/jsp/Student.java
@@ -0,0 +1,36 @@
+package com.gyojincompany.jsp;
+
+public class Student {
+	
+	private String name;//이름
+	private int age;//나이
+	private int grade;//학년
+	private int studentNumber;//학번	
+	
+	public String getName() { //getter
+		return name;
+	}
+	public void setName(String name) { //setter
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public int getStudentNumber() {
+		return studentNumber;
+	}
+	public void setStudentNumber(int studentNumber) {
+		this.studentNumber = studentNumber;
+	}
+	
+	
+}
